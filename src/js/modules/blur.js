97494a2cecf9009271ad383403b90e2974591215
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2017-09-18 18:54:55 +0300

    4pages ver. 1.1 changed header route

--- src/js/modules/blur.js
+++ src/js/modules/blur.js
@@ -1,10 +1,10 @@
 export const blur = (function() {
     var wrapper = document.querySelector('.feed-form__wrapper'),
-    form = document.querySelector('.feed');
+    form = document.querySelector('.form');
 
     return {
         set: function() {
-            var imgWidth = document.querySelector('.feed-form__blur').offsetWidth,
+            var imgWidth = document.querySelector('.feed-form__bg').offsetWidth,
                 posLeft = -wrapper.offsetLeft,
                 posTop = -wrapper.offsetTop,
                 blurCSS = form.style;
