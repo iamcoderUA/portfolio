d16c1fde85653c688a015380bfa4b7570511e29e
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2017-09-24 18:48:10 +0300

    fixed index height and btn

--- src/js/modules/auth.js
+++ src/js/modules/auth.js
@@ -1,14 +1,11 @@
 export let auth = {
-    btn : document.querySelector('.welcome-block__button'),
+    btn : document.querySelector('.welcome-block__link'),
     container : document.querySelector('.welcome-container'),
     goBack : document.querySelector('.welcome-auth__homelink'),
     icons : document.querySelectorAll('.front-info__svg'),
     rotate: function() {
         this.container.classList.add('active')
         this.btn.style.opacity = '0';
-        
-        // this.icons.style.opacity = '0';
-        // this.btn.style.backgroundColor = "none"
     },
     rotateBack: function() {
         this.container.classList.remove('active')
