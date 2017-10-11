7d5e2751e140501f1a28b7186bb41ff55347b6b0
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2017-10-11 23:45:39 +0300

    add api

--- myapp/app.js
+++ myapp/app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+require('./api/models/db');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
+const indexApi = require('./api/routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api', indexApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
