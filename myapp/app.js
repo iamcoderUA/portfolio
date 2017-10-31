6ef6214e78e6c2f5caaf3a17992f60b145de5838
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2017-11-01 00:24:40 +0200

    changed app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,60 +1,55 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const fs                        = require('fs');
+const path                      = require('path');
+const express                   = require('express');
+const MongoClient               = require("mongodb").MongoClient;
+const cookieParser              = require('cookie-parser');
+const logger                    = require('morgan');
+const bodyParser                = require('body-parser');
 require('./api/models/db');
 
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const mongoose                  = require('mongoose');
+const session                   = require('express-session');
+const MongoStore                = require('connect-mongo')(session);
+
+const app = express();
+const port = 80;
+let db;
+
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'pug');
 
 const index = require('./routes/index');
 const indexApi = require('./api/routes/index');
 
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
-app.use(logger('dev'));
+app.use(logger('dev'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use(session({
-  secret: 'frontend',
+  secret: 'weh649',
   cookie: {
     path: '/',
     httpOnly: true,
-    maxAge: null,
+    maxAge: null
   },
   saveUninitialized: false,
   resave: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}));
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
 
 app.use('/', index);
 app.use('/api', indexApi);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
 app.use((req, res, next) => res.status(404).send("Запрашиваемая вами страница не найдена"))
 
 app.use((err, req, res, next) => {
-  res.status(500);
-  res.render('error', {error: err.message});
-  console.error(err.message, err.stack);
+    res.status(500);
+    res.render('error', {error: err.message});
+    console.error(err.message, err.stack);
 })
 
-module.exports = app;
+app.listen(port, '0.0.0.0', () => {
+  console.log('запуск сервера на порту ' + port);
+})
